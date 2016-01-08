fbd7a92b70adb2cfecbaa5c6d0633bf395ca9672
Thomas Lohner <tl@scale.sc>
2016-01-08 12:59:30 +0100

    ensure /etc/etcd

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,5 +3,10 @@
 class etcd::install {
   if $::etcd::mange_package {
     package { 'etcd': ensure => $etcd::ensure, }
+  } else {
+    # make sure /etc/etcd is present
+	file {'/etc/etcd/':
+	  ensure => directory,
+	}
   }
 }
