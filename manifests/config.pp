6e4975205f9723715e8580af62bbdd11752002b9
Cristian Falcas <cristi.falcas@gmail.com>
2016-01-18 17:39:53 +0200

    add support for el6

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,8 +1,22 @@
 # == Class: etcd::config
 #
 class etcd::config {
-  file { '/etc/etcd/etcd.conf':
-    ensure  => 'file',
-    content => template("${module_name}/etc/etcd/etcd.conf.erb"),
+  case operatingsystemmajrelease {
+    6       : {
+      file { '/etc/sysconfig/etcd.conf':
+        ensure  => 'file',
+        content => template("${module_name}/etc/etcd/etcd.conf.erb"),
+      }
+    }
+    7       : {
+      file { '/etc/etcd/': ensure => 'directory', } ->
+      file { '/etc/etcd/etcd.conf':
+        ensure  => 'file',
+        content => template("${module_name}/etc/etcd/etcd.conf.erb"),
+      }
+    }
+    default : {
+      fail("Unsupported OS.")
+    }
   }
 }
