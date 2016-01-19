897b810b17d9d4a33234640d8e4b15790420115d
Karolis Labrencis <karolis@labrencis.lt>
2016-01-19 17:29:36 +0200

    Add support for Debian based systems

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,16 +1,29 @@
 # == Class: etcd::config
 #
 class etcd::config {
-  case $::operatingsystemmajrelease {
-    6       : {
-      file { '/etc/sysconfig/etcd.conf':
-        ensure  => 'file',
-        content => template("${module_name}/etc/etcd/etcd.conf.erb"),
+  case $::osfamily {
+    'RedHat' : {
+      case $::operatingsystemmajrelease {
+        6       : {
+          file { '/etc/sysconfig/etcd.conf':
+            ensure  => 'file',
+            content => template("${module_name}/etc/etcd/etcd.conf.erb"),
+          }
+        }
+        7       : {
+          file { '/etc/etcd/': ensure => 'directory', } ->
+          file { '/etc/etcd/etcd.conf':
+            ensure  => 'file',
+            content => template("${module_name}/etc/etcd/etcd.conf.erb"),
+          }
+        }
+        default : {
+          fail('Unsupported OS.')
+        }
       }
     }
-    7       : {
-      file { '/etc/etcd/': ensure => 'directory', } ->
-      file { '/etc/etcd/etcd.conf':
+    'Debian' : {
+      file { '/etc/default/etcd.conf':
         ensure  => 'file',
         content => template("${module_name}/etc/etcd/etcd.conf.erb"),
       }
