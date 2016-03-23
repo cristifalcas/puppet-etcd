b0c5388d016ac9c67ff710bf7baf5af009a03ed8
Cristian Falcas <cristi.falcas@gmail.com>
2016-03-23 18:57:19 +0200

    updates

--- lib/puppet/provider/etcd_key/etcdctl.rb
+++ lib/puppet/provider/etcd_key/etcdctl.rb
@@ -29,23 +29,27 @@ Puppet::Type.type(:etcd_key).provide(:etcdctl) do
   def create
     args = arguments
     args << 'set'  << @resource[:name] << @resource[:value]
+    debug "[etcd create]: etcdctl #{args}\n"
     etcdctl(args)
   end
 
   def destroy
     args = arguments
     args << 'rm'  << @resource[:name]
+    debug "[etcd rm]: etcdctl #{args}\n"
     etcdctl(args)
   end
 
   def value
     args = arguments
     args << 'get'  << @resource[:name]
+    debug "[etcd get]: etcdctl #{args}\n"
     etcdctl(args).chomp
   end
 
   def value=(val)
     args = arguments
+    debug "[etcd set]: etcdctl #{args}\n"
     args << 'set'  << @resource[:name] << val
     etcdctl(args)
   end
