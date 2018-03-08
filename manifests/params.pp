1c378f7bdf804c9aadbce5b42b081cae9b3f2992
Khanh Ngo <khanh.ngo@innogram.co>
2018-03-08 10:24:06 +0700

    Adjustment on directory creation to work with Ubuntu distribution

--- manifests/params.pp
+++ manifests/params.pp
@@ -20,7 +20,7 @@ class etcd::params {
       }
     }
     'Debian' : {
-      $config_file_path = '/etc/default/etcd.conf'
+      $config_file_path = '/etc/default/etcd'
     }
     default  : {
       fail('Unsupported OS.')
